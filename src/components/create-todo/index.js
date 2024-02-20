a6f7859e4df5f322044b5b683c52fe6081136bd1
Hannan-Laaraba <ashraweelaaraba@gmail.com>
2024-02-20 12:13:20 +0000

    updates

--- src/components/create-todo/index.js
+++ src/components/create-todo/index.js
@@ -7,32 +7,13 @@ function CreateToDo() {
 
     const [todo, setTodo] = useState("");
 
-     //function collectInput(event){
-       // todo= event.target.value;
-       // setTodo(event.target.value);
-     //}
-
-    // const collectInput = event => setTodo(event.target.value);
-    
-     //function saveTodo(){
-        //Get existing list of tools from local storage
-        //let tools = JSON.parse(localStorage.getItem("TODO_KEY")) || [];
-        //console.log(todos)
-        //Add a new todo to existing list of todos
-        //Set all todos in local storage
-        //setTodos([...todos,todo]);
         
         localStorage.setItem("TODO_KEY", JSON.stringify(todos))
-        //saveTodo
-    // }
-   // function saveTodo(event){
-        //console.log(event.target.value);
-    //}
+      
 
     return (
        <section className={styles.createTodoSection}> 
         <input 
-        //onChange={collectInput}
         onChange={event => setTodo(event.target.value)} 
          className={styles.createTodoInput}
         placeholder="Start typing....." />
--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -5,15 +5,6 @@ import TodoItem from "../todo-item";
 function TodoList() {
     const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
 
-    //let todos;
-    //function getTodos(){
-        //Get all todos from local storage and store it.
-       // let todos = JSON.parse(localStorage.getItem("TODO_KEY"));
-        //Update React State
-       // setTodos(todos);
-    
-
-   // useEffect(getTodos, []);
 
     return (
         <ul className= "list-group">
